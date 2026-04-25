01dcea9e96910c32d087f52c0f2eb54b28a1066f
Daniel Kowalski <daniel.kowalski@modelingevolution.com>
2026-04-25 11:10:16 +0200

    refactor: make PlayerRect internal (PR#11 finding #2)

--- src/BlazorBlaze.Server/NativePlayer/VideoSurfaceConstants.cs
+++ src/BlazorBlaze.Server/NativePlayer/VideoSurfaceConstants.cs
@@ -5,4 +5,4 @@ internal static class VideoSurfaceConstants
     internal const string ModulePath = "./_content/BlazorBlaze.Server/video-surface.js";
 }
 
-public readonly record struct PlayerRect(int X, int Y, int Width, int Height);
+internal readonly record struct PlayerRect(int X, int Y, int Width, int Height);
